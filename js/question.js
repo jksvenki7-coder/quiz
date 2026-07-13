1bcd3d856a4e7ec8c4a8286df8bfb26a3b5d6436
Freshtohome <jksvenki7@gmail.com>
2026-07-13 11:14:04 +0530

    Create questions array for quiz functionality
    Added an array of questions with options and answers for a quiz.

---
+++ js/question.js
@@ -0,0 +1,42 @@
+const questions = [
+
+{
+id:1,
+question:"V3 Group Founder Name?",
+options:[
+"Venkatesh",
+"Ramesh",
+"Suresh",
+"Mahesh"
+],
+answer:0,
+points:10
+},
+
+{
+id:2,
+question:"2 + 2 = ?",
+options:[
+"3",
+"4",
+"5",
+"6"
+],
+answer:1,
+points:10
+},
+
+{
+id:3,
+question:"India Capital?",
+options:[
+"Mumbai",
+"Hyderabad",
+"Delhi",
+"Chennai"
+],
+answer:2,
+points:10
+}
+
+];
